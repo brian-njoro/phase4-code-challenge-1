f2d69ea5be4132c3e392cd27f0e2b034c4b5c42a
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-09-24 20:42:35 +0300

    add get heroes route

--- python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/code-challenge/app/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,request,jsonify
 from flask_migrate import Migrate
 
-from models import db, Hero
+from models import db, Hero,Power,HeroPower
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
@@ -13,9 +13,16 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def home():
-    return ''
+# GET /heroes
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    heroes = Hero.query.all()
+    hero_list = [
+        {"id": hero.id, "name": hero.name, "super_name": hero.super_name}
+        for hero in heroes
+    ]
+    return jsonify(hero_list)
+
 
 
 if __name__ == '__main__':
--- python-code-challenge-superheroes/code-challenge/app/models.py
+++ python-code-challenge-superheroes/code-challenge/app/models.py
@@ -3,8 +3,16 @@ from sqlalchemy.orm import validates
 from datetime import datetime
 from sqlalchemy import CheckConstraint
 
-
 db = SQLAlchemy()
+
+# hero_powers = db.Table(
+#     'hero_powers',
+#     db.Column('hero_id', db.Integer, db.ForeignKey('hero.id'), primary_key=True),
+#     db.Column('power_id', db.Integer, db.ForeignKey('powers.id'), primary_key=True),
+#     db.Column('strength', db.String(255)),
+#     db.Column('created_at', db.DateTime, default=db.func.current_timestamp()),
+#     db.Column('updated_at', db.DateTime, default=db.func.current_timestamp())
+# )
 
 class Hero(db.Model):
     __tablename__ = 'hero'
@@ -14,10 +22,17 @@ class Hero(db.Model):
     super_name = db.Column(db.String)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    
-    # many-to-many relationship with Power table
-    powers = db.relationship('Power', secondary='hero_powers', back_populates='heroes')
 
+    hero_powers = db.relationship('HeroPower', back_populates='hero', cascade='all, delete-orphan')
+
+    powers = db.relationship(
+        'Power',
+        secondary='hero_powers',
+        back_populates='heroes',
+        viewonly=True,
+        primaryjoin="Hero.id == hero_powers.c.hero_id",
+        secondaryjoin="Power.id == hero_powers.c.power_id",
+    )
 
 class Power(db.Model):
     __tablename__ = 'powers'
@@ -27,25 +42,33 @@ class Power(db.Model):
     description = db.Column(db.String)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    
+
     @validates('description')
     def validate_description(self, key, description):
         if len(description) < 20:
             raise ValueError('Description must be at least 20 characters long')
         return description
 
-    #many-to-many relationship with Hero
-    heroes = db.relationship('Hero', secondary='hero_powers', back_populates='powers')
+    hero_powers = db.relationship('HeroPower', back_populates='power', cascade='all, delete-orphan')
+
+    heroes = db.relationship(
+        'Hero',
+        secondary='hero_powers',
+        back_populates='powers',
+        viewonly=True,
+        primaryjoin="Power.id == hero_powers.c.power_id",
+        secondaryjoin="Hero.id == hero_powers.c.hero_id",
+    )
 
 class HeroPower(db.Model):
     __tablename__ = 'hero_powers'
 
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String, CheckConstraint('strength IN ("Strong", "Weak", "Average")'))
-    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'))
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    
 
-# add any models you may need. 
+    hero = db.relationship('Hero', back_populates='hero_powers', primaryjoin=hero_id == Hero.id)
+    power = db.relationship('Power', back_populates='hero_powers', primaryjoin=power_id == Power.id)
