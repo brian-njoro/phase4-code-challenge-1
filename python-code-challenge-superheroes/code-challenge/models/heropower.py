aa02ae79e3b8fb759392a3f8cd56d0552a00ff8f
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-09-28 07:41:58 +0300

    add strength valiodation

--- python-code-challenge-superheroes/code-challenge/models/heropower.py
+++ python-code-challenge-superheroes/code-challenge/models/heropower.py
@@ -1,12 +1,14 @@
 from .dbconfig import db
 from sqlalchemy import CheckConstraint
 from datetime import datetime
+from sqlalchemy.orm import validates
+
 
 class HeroPower(db.Model):
     __tablename__ = 'hero_powers'
 
     id = db.Column(db.Integer, primary_key=True)
-    strength = db.Column(db.String, CheckConstraint('strength IN ("Strong", "Weak", "Average")'))
+    strength = db.Column(db.String)
     hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'))
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
@@ -14,6 +16,14 @@ class HeroPower(db.Model):
 
     hero = db.relationship('Hero', backref='hero_powers')
     power = db.relationship('Power', backref='hero_powers')
+
+    @validates('strength')
+    def validate_strength(self, key, value):
+        strength_level = ['Strong', 'Weak', 'Average']
+        if value not in strength_level:
+            raise ValueError("Invalid strength value")
+        return value
+
 
     def __repr__(self):
         return f"<HeroPower(id={self.id}, strength='{self.strength}', hero_id={self.hero_id}, power_id={self.power_id})>"
